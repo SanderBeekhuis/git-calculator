ce2408ca1a72c9665299adc6cb0e4f3e68c25234
Sander Beekhuis <16463246+SanderBeekhuis@users.noreply.github.com>
2023-08-29 16:23:36 +0200

    Add multiplication method

--- calc/main.py
+++ calc/main.py
@@ -2,5 +2,10 @@ def add(a, b):
     return a + b
 
 
+def mult(a, b):
+    return a * b
+
+
 if __name__ == '__main__':
-    print(f'2 + 3 = {add(2,3)}')
+    print(f'2 + 3 = {add(2, 3)}')
+    print(f'2 * 3 = {mult(2, 3)}')
--- calc/test_main.py
+++ calc/test_main.py
@@ -3,3 +3,8 @@ import calc
 
 def test_add():
     assert calc.add(2, 3) == 5
+
+
+def test_mult():
+    assert calc.mult(2, 3) == 6
+    assert calc.mult(2, 0) == 0

ac6d577ae81b09cde4568f95100d4402ea9eca01
G. Nutma <43369468+GNutma@users.noreply.github.com>
2023-09-18 11:37:00 +0200

    Added product function to the mult()

--- calc/test_main.py
+++ calc/test_main.py
@@ -10,6 +10,8 @@ def test_add():
 def test_mult():
     assert calc.mult(2, 3) == 6
     assert calc.mult(2, 0) == 0
+    assert calc.mult(2, 3, 4) == 24
+    assert calc.mult(2) == 2
 
 
 def test_subtraction():

3704797976582ea6e74807723da703712e6b4dba
Roel Adriaans <roel@adriaans.org>
2024-02-13 15:05:04 +0100

    Implement sub

--- calc/main.py
+++ calc/main.py
@@ -5,5 +5,10 @@ def add(*args):
     return sum(args)
 
 
+def sub(a, b):
+    return a - b
+
+
 if __name__ == "__main__":
     print(f"2 + 3 = {add(2, 3)}")
+    print(f"5 - 2 = {sub(5, 2)}")
--- calc/test_main.py
+++ calc/test_main.py
@@ -6,3 +6,9 @@ def test_add():
     assert calc.add(2, 3) == 5
     assert calc.add(2, 3, 4) == 9
     assert calc.add(2) == 2
+
+
+def test_sub():
+    assert calc.sub(5, 3) == 2
+    assert calc.sub(0, 0) == 0
+    assert calc.sub(10, 20) == -10

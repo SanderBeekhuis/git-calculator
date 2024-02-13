99779a3aeeb553c6174a24f9ebbc1fc386d1d76c
Roel Adriaans <roel@adriaans.org>
2024-02-13 15:01:26 +0100

    Cleanup

--- calc/main.py
+++ calc/main.py
@@ -5,31 +5,5 @@ def add(*args):
     return sum(args)
 
 
-def subtract(a, b):
-    return a - b
-
-
-def mult(*args: float | int) -> float | int:
-    """Multiply a list of numbers together
-
-    Args:
-        *args (float | int): A list of numbers to multiply
-
-    Returns:
-        float | int: The product of the numbers
-    """
-    return prod(args)
-
-
-def division(a: int, b: int) -> float:
-    """Divides a by b and returns the result.
-    """
-    if b == 0:
-        raise ZeroDivisionError
-    return a / b
-
-
-if __name__ == '__main__':
-    print(f'2 + 3 = {add(2, 3)}')
-    print(f'3 - 1 = {subtract(3, 1)}')
-    print(f'2 * 3 = {mult(2, 3)}')
+if __name__ == "__main__":
+    print(f"2 + 3 = {add(2, 3)}")
--- calc/test_main.py
+++ calc/test_main.py
@@ -6,22 +6,3 @@ def test_add():
     assert calc.add(2, 3) == 5
     assert calc.add(2, 3, 4) == 9
     assert calc.add(2) == 2
-
-
-def test_mult():
-    assert calc.mult(2, 3) == 6
-    assert calc.mult(2, 0) == 0
-    assert calc.mult(2, 3, 4) == 24
-    assert calc.mult(2) == 2
-    assert calc.mult(3, .5) == 1.5
-
-
-def test_subtraction():
-    assert calc.subtract(3, 1) == 2
-
-
-def test_division():
-    assert calc.division(4, 2) == 2.0
-    assert calc.division(5, 3) == 5/3
-    with pytest.raises(ZeroDivisionError):
-        calc.division(1, 0)
